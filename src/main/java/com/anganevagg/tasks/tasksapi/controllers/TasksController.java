6aa2a70a8aad287dc7124588e7f5f59ed98d0491
anganevagg <anganevagg@protonmail.com>
2023-09-07 20:27:02 -0600

    feature: rutas creadas

--- src/main/java/com/anganevagg/tasks/tasksapi/controllers/TasksController.java
+++ src/main/java/com/anganevagg/tasks/tasksapi/controllers/TasksController.java
@@ -1,12 +1,35 @@
 package com.anganevagg.tasks.tasksapi.controllers;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TasksController {
-    @GetMapping("/api/hello")
-    public String hello(){
-        return "Hello world";
+    @GetMapping("/api/tasks")
+    public String getAll(){
+        return "Get all routes";
+    }
+
+    @GetMapping("/api/tasks/{id}")
+    public String getById(){
+        return "Get by id";
+    }
+
+    @PostMapping("/api/tasks")
+    public String create(){
+        return "Create";
+    }
+
+    @PatchMapping("/api/tasks/{id}")
+    public String update(){
+        return "Update";
+    }
+
+    @DeleteMapping("/api/tasks/{id}")
+    public String delete(){
+        return "Delete";
     }
 }
